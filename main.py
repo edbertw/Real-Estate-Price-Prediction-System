efa92c60e0a02dedfa6ef9da6f67407af44ffc39
Edbert Widjaja <157877834+edbertw@users.noreply.github.com>
2024-06-23 19:01:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#Import necessary libraries and functions
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
@@ -7,19 +9,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
+from sklearn.cluster import KMeans
+from sklearn.metrics import mean_absolute_error, r2_score
 
-
-X = pd.read_csv('houses.csv', index_col='Id')
+#Make Target and Predictors
+X = pd.read_csv('/kaggle/input/houses-csv/houses.csv', index_col='Id')
 print(X.isnull().sum())
 X.dropna(axis=0, subset=['SalePrice'], inplace=True)
 y = X.SalePrice              
 X.drop(['SalePrice'], axis=1, inplace=True)
 
+#Engineer a "Cluster" feature
 features = ["LotArea","TotalBsmtSF","1stFlrSF","2ndFlrSF","GrLivArea"]
 X_scaled = X.loc[:, features]
 X_scaled = (X_scaled - X_scaled.mean(axis=0)) / X_scaled.std(axis=0)
-kmeans = KMeans(n_clusters = 10,n_init = 10, random_state=0)
+
+#Tune the Cluster feature
+wcss = []
+for i in range(3,14):
+    kmeans = KMeans(n_clusters = i, random_state = 0)
+    kmeans.fit(X_scaled)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(3,14),wcss)
+plt.xlabel("Number of Clusters (k)")
+plt.ylabel("WCSS")
+plt.title('Elbow Method')
+plt.show()
+
+#Elbow WCSS Method shows 5 clusters
+kmeans = KMeans(n_clusters = 5,n_init = 10, random_state=0)
 X["Cluster"] = kmeans.fit_predict(X_scaled)
 
 X_trainN, X_testN, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2,
@@ -30,10 +48,12 @@ low_cardinality_cols = [cname for cname in X_trainN.columns if X_trainN[cname].n
 
 numeric_cols = [cname for cname in X_trainN.columns if X_trainN[cname].dtype in ['int64', 'float64']]
 
+#Removing columns with high cardinality, for simplicity in one-hot encoding
 cols = low_cardinality_cols + numeric_cols
-X_train = X_trainN[cols].copy() #Removing columns with high cardinality, for simplicity in one-hot encoding
+X_train = X_trainN[cols].copy() 
 X_test = X_testN[cols].copy()
 
+# Construct the model pipeline
 numerical_transformer = SimpleImputer(strategy='constant')
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='most_frequent')),
@@ -48,6 +68,7 @@ preprocessor = ColumnTransformer(
 model = RandomForestRegressor(random_state = 0)
 Pipe = Pipeline(steps=[('preprocessor', preprocessor),
                       ('model', model)])
+# Implementation of GridSearchCV for hyperparameter tuning
 param_grid = {
     'model__n_estimators':[100,200,300],
     'model__max_depth':[5,10,15],
@@ -61,10 +82,17 @@ best_score = grid_search.best_score_
 print(best_parameters)
 print(best_score)
 
+# Evaluation of model
 best_model = grid_search.best_estimator_
 y_pred = best_model.predict(X_test)
 print("MAE: ",mean_absolute_error(y_pred, y_test))
+r2 = r2_score(y_test, y_pred)
+print(f"R-Squared score: {r2:.2f}")
+acc = r2 * 100
+acc = round(acc)
+print(f"Accuracy: {acc}%")
 
+# Output predictions
 output = pd.DataFrame({'Id': X_test.index,
                       'Predicted Sale Price': y_pred})
 print(output.head())
